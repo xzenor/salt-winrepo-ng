182bea8f3412edb775a3ebacb9c59ffed7ac627d
xzenor <15305998+xzenor@users.noreply.github.com>
2023-08-29 16:38:15 +0200

    Delete patchmypc directory

--- patchmypc/set-new-path.ps1
+++
@@ -1,6 +0,0 @@
-
-$OLDPATH = [System.Environment]::GetEnvironmentVariable('PATH','machine')
-$INCLUDE = "$env:ProgramData\PatchMyPC"
-$NEWPATH = "$OLDPATH;$INCLUDE"
-[Environment]::SetEnvironmentVariable("PATH", "$NEWPATH", "Machine")
-
